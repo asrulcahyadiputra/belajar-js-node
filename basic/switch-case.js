865008367ac6589fec5061802eacc951ccf13bf5
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-05-13 18:23:50 +0800

    chore: switch case

---
+++ basic/switch-case.js
@@ -0,0 +1,34 @@
+//  switch case
+let helloByName = "John";
+
+switch (helloByName) {
+  case "John":
+    console.log("Hello John");
+    break;
+  case "Jane":
+    console.log("Hello Jane");
+    break;
+  case "Jack":
+    console.log("Hello Jack");
+    break;
+  default:
+    console.log("Hello Stranger");
+    break;
+}
+
+let drink = "coffee";
+
+switch (drink) {
+  case "coffee":
+    console.log("I like coffee");
+    break;
+  case "tea":
+    console.log("I like tea");
+    break;
+  case "milk":
+    console.log("I like milk");
+    break;
+  default:
+    console.log("I don't like anything");
+    break;
+}

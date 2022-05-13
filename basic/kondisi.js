cc0beef5fbab467f5bc8d07853afd2a67b500d92
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-05-13 18:15:49 +0800

    chore: if else

---
+++ basic/kondisi.js
@@ -0,0 +1,17 @@
+// if else
+let angka = 1;
+
+if (angka == 1) {
+  console.log("Angka 1");
+} else {
+  console.log("Angka bukan 1");
+}
+
+// example
+let umur = 17;
+
+if (umur >= 17) {
+  console.log("Anda boleh mengambil KTP");
+} else {
+  console.log("Anda tidak boleh mengambil KTP");
+}

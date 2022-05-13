0456a14833d9e0daa7358120b8c1f2004c0ccbe2
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-05-13 17:41:14 +0800

    chore: operator matematika dan logika

---
+++ basic/operator.js
@@ -0,0 +1,25 @@
+// operator matematika
+
+let umur = 17;
+
+// penjumlahan
+let jumlah = umur + 1;
+
+// pengurangan
+let kurang = umur - 1;
+
+// perkalian
+let kali = umur * 2;
+
+// pembagian
+let bagi = umur / 2;
+
+// urutan operasi
+let urutan = 1 + 2 * 3; // = 7
+let urutKali = (1 * 2) / 3;
+
+// operator logila
+let operatorLogika = true && false; // false
+let operatorLogika2 = true || false; // true
+let operatorLogika3 = !true; // false
+let opratorLogika4 = true == "true"; // true

7cf92c429c997a46e629b3f0e9d0dc9a3867700d
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-05-13 18:34:19 +0800

    chore: nested if

---
+++ basic/nested-if.js
@@ -0,0 +1,13 @@
+// nested if
+let umur = 17;
+let tinggiBadan = 155;
+
+if (umur < 17) {
+  if (tinggiBadan < 155) {
+    console.log("tidak boleh masuk");
+  } else {
+    console.log("boleh masuk");
+  }
+} else {
+  console.log("boleh masuk");
+}
